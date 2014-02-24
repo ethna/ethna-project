ea2549f6c46505aa78ec00b968c8d6ee8c3750cc
Shuhei Tanuma <shuhei.tanuma@gree.co.jp>
2014-02-24 13:45:26 +0900

    dropping layout support

--- app/Example_ViewClass.php
+++ app/Example_ViewClass.php
@@ -1,10 +1,5 @@
 <?php
 class Example_ViewClass extends Ethna_ViewClass
 {
-    /** @var  string  set layout template file   */
-    public $_layout_file = 'layout';
-
-    /** @var boolean  layout template use flag   */
-    public $use_layout = true;
 }
 
